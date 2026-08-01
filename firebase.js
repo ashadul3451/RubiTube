177b666cfac55267a21843c97277cafb534d89bb
ashadul3451 <ashadul.islam3451@gmail.com>
2026-08-01 15:13:32 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyArOr4ef6mLGvA4uINejFNfjyejNo4DcwI",
   authDomain: "rubitube.firebaseapp.com",
   projectId: "rubitube",
   storageBucket: "rubitube.firebasestorage.app",
